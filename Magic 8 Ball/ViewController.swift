486814e169a43fac5b769c7b5a8a0179de20c1a7
Pamela Needle <Pamneedle@Pamelas-MacBook-Pro.local>
2015-08-11 17:53:56 +1000

    lab 1 ios

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Pamela Needle on 8/11/15.
+//  Copyright (c) 2015 Pamela Needle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Magic 8 BallTests/Magic_8_BallTests.swift
@@ -0,0 +1,36 @@
+//
+//  Magic_8_BallTests.swift
+//  Magic 8 BallTests
+//
+//  Created by Pamela Needle on 8/11/15.
+//  Copyright (c) 2015 Pamela Needle. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Magic_8_BallTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
