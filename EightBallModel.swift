3cc1209d7fede9f4ca0f71aaadb7f51261958a8c
Pamela Needle <Pamneedle@Pamelas-MacBook-Pro.local>
2015-08-13 14:54:45 +1000

    lab1 finish

---
+++ EightBallModel.swift
@@ -0,0 +1,56 @@
+//
+//  EightBallModel.swift
+//  Magic 8 Ball
+//
+//  Created by Pamela Needle on 8/11/15.
+//  Copyright (c) 2015 Pamela Needle. All rights reserved.
+//
+
+import Foundation
+
+class EightBallModel : Printable, DebugPrintable{
+    let initialResponseArray : Array<String>
+    var responseArray : [String]?
+
+    init() {
+        initialResponseArray = ["Yes","No"]
+        responseArray = initialResponseArray
+    }
+
+    init(extraResponseArray : Array<String>){
+        initialResponseArray = ["Yes","No"]
+        responseArray = initialResponseArray
+        for str in extraResponseArray {
+            responseArray!.append(str)
+        }
+       
+        }
+    
+    func generateResponse() -> String {
+        let numOfResponses = UInt32(responseArray!.count)
+        let randomIndex = arc4random_uniform(numOfResponses)
+        let ranInt = Int(randomIndex)
+        return responseArray![ranInt]
+    }
+  
+
+
+var description : String {
+    //Use a for loop to create a string based on the elements inside of response array
+    var responseStrings = ""
+    for resp in responseArray! {
+        responseStrings += resp
+    }
+    return responseStrings
+}
+
+var debugDescription :String {
+    //Use a for loop to create a string based on the elements inside of response array
+    //Make sure the string starts with the word "Debug:"
+    var debugStrings = "Debug:"
+    for resp in responseArray!{
+        debugStrings += resp
+    }
+    return debugStrings
+}
+}
--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("Pamela Needle")
+        var age = 20.00
+        println("My age is \(age)")
+        var name = "Pamela Needle"
+        println(name)
+        let responses = ["Not a chance!","Perhaps","Most definetely","Maybe","For sure","Yeah right!"]
+        let model = EightBallModel(extraResponseArray:responses)
+        println("Will I get full marks for this lab?")
+        println(model.generateResponse())
+        println("Will the Cronulla sharks receive a premiership this year?")
+        println(model.generateResponse())
+        println("Will I end up becoming a cat person when I get old?")
+        println(model.generateResponse())
         // Do any additional setup after loading the view, typically from a nib.
     }
 
